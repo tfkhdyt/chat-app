63355562c20df5faddc0f8f7d4fcfe5fcd7f258d
Taufik Hidayat <tfkhdyt@proton.me>
2024-08-07 11:03:20 +0700

    server: fix error on username and email verification

--- server/user/user_repository.go
+++ server/user/user_repository.go
@@ -30,12 +30,9 @@ func (r *userRepoPg) create(user *User) error {
 func (r *userRepoPg) verifyUsernameAvailability(username string) error {
 	var user User
 
-	if err := r.db.Where("username = ?", username).First(&user).Error; err != nil {
-		return exception.NewInternalServerError("failed to verify username availability", err)
-	}
-
-	if user.ID != 0 {
+	if err := r.db.Select("id").Where("username = ?", username).First(&user).Error; err == nil {
 		return exception.NewBadRequestError("username is already taken", nil)
+
 	}
 
 	return nil
@@ -44,11 +41,7 @@ func (r *userRepoPg) verifyUsernameAvailability(username string) error {
 func (r *userRepoPg) verifyEmailAvailability(email string) error {
 	var user User
 
-	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
-		return exception.NewInternalServerError("failed to verify email availability", err)
-	}
-
-	if user.ID != 0 {
+	if err := r.db.Select("id").Where("email = ?", email).First(&user).Error; err == nil {
 		return exception.NewBadRequestError("email is already taken", nil)
 	}
 
